70b0c44bdc5d96d7d03fdd463d43f26757dc40d1
davidjobrant <da2311jo-s@student.lu.se>
2023-03-13 11:12:36 +0000

    v1 of rttd script

--- RTTD/rttd.py
+++ RTTD/rttd.py
@@ -1,73 +1,85 @@
 import json 
 from datetime import datetime, timedelta
 import pyufw as ufw
+import schedule 
+import time
 
-#ufw = ""
-
+'''
+    Retrieves all IPs seen within the last two days. 
+'''
 def load_honeypot_data():
-    # Reading the honeypot data from the json file 
     data = []
+
+    # Retrieves the honeypot data from today
     for line in open('/home/cowrie/cowrie/var/log/cowrie/cowrie.json', 'r'):
         data.append(json.loads(line))
 
-    # TODO we need to find yesterdays date, because a new file seems to be added for each day. 
-    for line in open('/home/cowrie/cowrie/var/log/cowrie/cowrie.json', 'r'):
+    # Retrieves the honeypot data from yesterday
+    yesterday = get_yesterday()
+    yesterday_file_path = f'/home/cowrie/cowrie/var/log/cowrie/cowrie.json.{yesterday}'
+    for line in open(yesterday_file_path, 'r'):
         data.append(json.loads(line))
 
-
-    #print(data)
-    # map containing ip as key and timestamp as value 
+    # Dictionary containing IPs as keys and timestamps as values
     ip_timestamps = {}
 
     for entry in data:
         ip_timestamps[entry['src_ip']] = entry['timestamp']
-    print(f"IPs with timestamps: {ip_timestamps}")
 
     return ip_timestamps
 
 '''
-    Retrieves two lists, one containing ips to be added, one containing ips to be deleted from the firewall
+    Retrieves the data of yesterday. 
+'''
+def get_yesterday(frmt='%Y-%m-%d', string=True):
+    yesterday = datetime.now() - timedelta(1)
+    if string:
+        return yesterday.strftime(frmt)
+    return yesterday
+
+'''
+    Retrieves two lists, one containing IPs to be added, one containing IPs to be deleted from the firewall. 
 '''
 def get_add_delete():
     ip_timestamps = load_honeypot_data()
     to_be_added = set()
     to_be_deleted = set()
-    print(len(ip_timestamps))
-    for k,v in ip_timestamps.items(): 
-        ip = k
-        timestamp = v
+
+    now = datetime.now()
+
+    for ip, timestamp in ip_timestamps.items(): 
         datetime_object = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
-        now = datetime.now()
-        # checks if the ip has accessed the honeypot within the last 24 hours
+
+        # Checks if the IP has accessed the honeypot within the last 24 hours
         if now-timedelta(hours=24) <= datetime_object <= now: 
-            print(f"{ip} should be added as a rule")
-            to_be_added.add(k)
+            print(f"ADD RULE: {ip}")
+            to_be_added.add(ip)
         else: 
-            print(f"{ip} should be deleted as a rule")
-            to_be_deleted.add(k)
-    print(len(to_be_added))
+            print(f"DELETE RULE: {ip}")
+            to_be_deleted.add(ip)
     return to_be_added, to_be_deleted
 
 '''
-    Retrieves firewall rules currently active
+    Retrieves all firewall rules currently active.
 '''
 def get_firewall_rules():
     return ufw.get_rules()
 
 '''
-    Retrieves the rules which should be deleted 
+    Retrieves the rules which should be deleted.
 '''
 def get_rules_tbd(tbd): 
     active_rules = get_firewall_rules() 
-    print(active_rules)
-    print(tbd)
-
     to_be_deleted = set()
-    # Find the numbers of the rules which needs to be deleted 
+
+    # Find the rule numbers of the rules which needs to be deleted 
     for number, rule in active_rules.items(): 
         if any(ip in rule for ip in tbd):
-            print("")
             to_be_deleted.add(number)
+
+    before_length = len(tbd)
+    after_length = len(to_be_deleted)
+    print(f"Out of {before_length} rules to delete, {after_length} are currently active")
 
     return to_be_deleted
 
@@ -87,10 +99,23 @@ def add_rules(ips):
         ufw.add(f"deny from {ip} to any port 22")
 
 '''
-    Runs the script
-    TODO: The script should be run every 5-10 minutes or so! 
+    Runs the script.
+    TODO: The script should be run every 5-10 minutes. Run with crontab instead of scheduling? 
 '''
 def main(): 
+    update_rules()
+
+    schedule.every(1).minutes.do(update_rules)
+
+    while True: 
+        schedule.run_pending()
+        time.sleep(1)
+
+'''
+    Retrieves the IPs seen in the honeypot over the last 24 hours,
+    then compares them with the rules currently active on ufw to be able to add and delete them accordingly.   
+'''
+def update_rules():
     add, delete = get_add_delete() 
     delete_rules(delete) 
     add_rules(add)
