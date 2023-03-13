95a90233bf5dab52b8b916ccf46c35570733b701
davidjobrant <da2311jo-s@student.lu.se>
2023-03-13 14:01:11 +0000

    cleanup, added block for port 443

--- RTTD/rttd.py
+++ RTTD/rttd.py
@@ -4,10 +4,11 @@ import pyufw as ufw
 import schedule 
 import time
 
-'''
-    Retrieves all IPs seen within the last two days. 
-'''
+
 def load_honeypot_data():
+    '''
+        Retrieves all IPs seen within the last two days. 
+    '''
     data = []
 
     # Retrieves the honeypot data from today
@@ -27,19 +28,21 @@ def load_honeypot_data():
 
     return ip_timestamps
 
-'''
-    Retrieves the date of yesterday. 
-'''
+
 def get_yesterday(frmt='%Y-%m-%d', string=True):
+    '''
+        Retrieves the date of yesterday. 
+    '''
     yesterday = datetime.now() - timedelta(1)
     if string:
         return yesterday.strftime(frmt)
     return yesterday
 
-'''
-    Retrieves two lists, one containing IPs to be added, one containing IPs to be deleted from the firewall. 
-'''
+
 def get_add_delete():
+    '''
+        Retrieves two lists, one containing IPs to be added, one containing IPs to be deleted from the firewall. 
+    '''
     ip_timestamps = load_honeypot_data()
     to_be_added = set()
     to_be_deleted = set()
@@ -51,27 +54,30 @@ def get_add_delete():
 
         # Checks if the IP has accessed the honeypot within the last 24 hours
         if now-timedelta(hours=24) <= datetime_object <= now: 
-            print(f"ADD RULE: {ip}")
+            #print(f"ADD RULE: {ip}")
             to_be_added.add(ip)
         else: 
-            print(f"DELETE RULE: {ip}")
+            #print(f"DELETE RULE: {ip}")
             to_be_deleted.add(ip)
     return to_be_added, to_be_deleted
 
-'''
-    Retrieves all firewall rules currently active.
-'''
+
 def get_firewall_rules():
+    '''
+        Retrieves all firewall rules currently active.
+    '''
     return ufw.get_rules()
 
-'''
-    Retrieves the rules which should be deleted.
-'''
+
 def get_rules_tbd(tbd): 
+    '''
+        Retrieves the rules which should be deleted.
+    '''
     active_rules = get_firewall_rules() 
     to_be_deleted = set()
 
-    # Find the rule numbers of the rules which needs to be deleted 
+    # Find the rule numbers of the rules which needs to be deleted
+    # University IP is whitelisted 130.208.240.12
     for number, rule in active_rules.items(): 
         if any(ip in rule for ip in tbd if not '130.208.240.12'):
             # todo: check if the ip is the university ip 
@@ -83,40 +89,43 @@ def get_rules_tbd(tbd):
 
     return to_be_deleted
 
-'''
-    Deletes firewall rules based on an input IP list.
-''' 
+
 def delete_rules(ips): 
+    '''
+        Deletes firewall rules based on an input IP list.
+    ''' 
     rules = get_rules_tbd(ips)
     for rule in rules: ufw.delete(rule)
 
-'''
-    Adds firewall rules based on an input IP list. If they're already in the firewall, they won't be duplicated. 
-'''
 def add_rules(ips):
-    for ip in ips: ufw.add(f"deny from {ip} to any port 22")
+    '''
+        Adds firewall rules based on an input IP list. If they're already in the firewall, they won't be duplicated. 
+    '''
+    for ip in ips: 
+        ufw.add(f"deny from {ip} to any port 80")
+        ufw.add(f"deny from {ip} to any port 443")
 
-'''
-    Runs the script.
-    TODO: The script should be run every 5-10 minutes. Run with crontab instead of scheduling? 
-'''
 def main(): 
-    update_rules()
-
+    '''
+        Runs the script every 5 minutes.
+    '''
     schedule.every(5).minutes.do(update_rules)
 
     while True: 
         schedule.run_pending()
         time.sleep(1)
 
-'''
-    Retrieves the IPs seen in the honeypot over the last 24 hours,
-    then compares them with the rules currently active on ufw to be able to add and delete them accordingly.   
-'''
 def update_rules():
+    '''
+        Retrieves the IPs seen in the honeypot over the last 24 hours,
+        then compares them with the rules currently active on ufw to be able to add and delete them accordingly.   
+    '''
+    start = time.time()
     add, delete = get_add_delete() 
     delete_rules(delete) 
     add_rules(add)
+    end = time.time()
+    print("Time:", end - start)
 
 if __name__=="__main__":
     main()
