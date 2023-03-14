cc8cff12587682ac75b642da9c2152c6b28aed54
Jonas Hagg <jonas.hagg@tum.de>
2023-03-14 15:19:49 +0000

    Log analyzer for ssh logs

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,4 +1,6 @@
-from datetime import datetime
+import json
+import os
+from datetime import datetime, timedelta
 from typing import List, Tuple, Dict
 
 from dateutil import parser
@@ -6,8 +8,9 @@ from dateutil import parser
 
 class AbstractAnalyzer:
     def __init__(self):
-        self.start = datetime.strptime('12-03-2023', '%d-%m-%Y')
-        self.end = datetime.strptime('13-03-2023', '%d-%m-%Y')
+        self.start = datetime.strptime('10-03-2023', '%d-%m-%Y')
+        self.end = datetime.strptime('11-03-2023', '%d-%m-%Y')
+        self.whitelisted_ips = ['130.208.240.12', '85.220.40.135', '129.187.205.52', '129.187.207.88']
 
     def total_connections(self) -> int:
         """
@@ -64,17 +67,18 @@ class AbstractAnalyzer:
 
 
 class HttpsAnalyzer(AbstractAnalyzer):
+    log_file = "example_logs/https_logs"
     def timestamps_per_ip(self) -> Dict[str, List[datetime]]:
         data = dict()
 
-        with open("example_logs/https_logs", "r") as f:
+        with open(self.log_file, "r") as f:
             lines = f.readlines()
 
         for line in lines:
             ip, timestamp, *_ = line.split("\t")
             timestamp = parser.parse(timestamp)
 
-            if timestamp > self.end or timestamp < self.start:
+            if timestamp > self.end or timestamp < self.start or ip in self.whitelisted_ips:
                 continue
 
             if ip in data:
@@ -85,10 +89,61 @@ class HttpsAnalyzer(AbstractAnalyzer):
         return data
 
 
-a = HttpsAnalyzer()
-print(a.timestamps_per_ip())
-print(a.total_connections())
-print(a.connections_per_day())
-print(a.unique_ips())
-print(a.ips_connected_multiple())
+class HttpAnalyzer(HttpsAnalyzer):
+    log_file = "example_logs/http_logs"
+
+
+class SSHAnalyzer(AbstractAnalyzer):
+    log_folder = "example_logs/cowrie"
+
+    def _log_files(self):
+        """
+        Returns all log files for the defined time frame.
+        """
+        log_files = []
+        delta = self.end - self.start
+        for i in range(delta.days):
+            day = self.start + timedelta(days=i)
+            formatted_day = day.strftime("%Y-%m-%d")
+            file = f"{self.log_folder}/cowrie.json.{formatted_day}"
+            if os.path.isfile(file):
+                log_files.append(file)
+        return log_files
+
+    def timestamps_per_ip(self) -> Dict[str, List[datetime]]:
+        data = dict()
+
+        log_files = self._log_files()
+        for log_file in log_files:
+            with open(log_file, "r") as f:
+                lines = f.readlines()
+
+            for line in lines:
+                event = json.loads(line)
+                timestamp = parser.parse(event["timestamp"]).replace(tzinfo=None)
+                ip = event["src_ip"]
+
+                if event["eventid"] != "cowrie.session.connect":
+                    continue
+
+                if timestamp > self.end or timestamp < self.start or ip in self.whitelisted_ips:
+                    continue
+
+                if ip in data:
+                    data[ip].append(timestamp)
+                else:
+                    data[ip] = [timestamp]
+
+        return data
+
+
+
+for service in [SSHAnalyzer(), HttpsAnalyzer(), HttpAnalyzer()]:
+
+    print(service.__class__)
+    print("Total connections:\t", service.total_connections())
+    print("Connections/day:\t", service.connections_per_day())
+    print("Unique IPs:\t\t", service.unique_ips())
+    print("Returning IPs:\t\t", service.ips_connected_multiple())
+    print()
 
