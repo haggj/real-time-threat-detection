c7b7198fe0bc13a1b2d9f0dca221307d12f19970
davidjobrant <>
2023-03-15 12:40:46 +0000

    Saved logs, finalized script

--- RTTD/rttd.py
+++ RTTD/rttd.py
@@ -1,23 +1,152 @@
-import json 
-from datetime import datetime, timedelta
-import pyufw as ufw
-import schedule 
+import sys
 import time
+import logging
+from threading import Thread
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler, FileSystemEventHandler
+import pyufw as ufw
+import json
+from datetime import datetime, timedelta
+from collections import defaultdict
+import schedule
+from ip_analyzer import IpAnalyzer
+import os
+
+'''
+    Script that runs based on updates of the cowrie logs. 
+    Blocks IP addresses, idea is to make it as much in real-time as possible.
+'''
+
+#WHITELISTED_IPS = ['130.208.240.12', '85.220.40.135']
+WHITELISTED_IPS = []
+cached_rules = defaultdict(list)
+
+def update_cached_rules():
+    '''
+        Updates the cached rules through retrieving the current firewall rules.
+    '''
+    print("------------- CACHING RULES -------------")
+    start = time.time()
+
+    rules = ufw.get_rules()
+
+    for number, rule in rules.items(): 
+        split = rule.split(" ")
+        cached_rules[split[2]] = number
+
+    end = time.time()
+    print("Time elapsed:", end - start)
+    print("--------- UPDATED CACHED RULES ----------\n")
 
 
-def load_honeypot_data():
+class MyEventHandler(FileSystemEventHandler):
+    '''
+        Handles events using Watchdog. 
+    '''
+
+    def __init__(self, path):
+        self.last_ip = ""
+        self.path = path
+
+    def on_modified(self, event):
+        '''
+            Called whenever a file is modified.
+        '''
+        handle_new_event(self.path)
+        
+def handle_new_event(path):
+    print("--------------- NEW ACCESS --------------")
+
+    start = time.time()
+    ip = load_last_event(path) # Grabs the IP
+    print("IP", ip)
+
+    # Check IP against cached rules 
+    if ip and ip not in cached_rules and ip not in WHITELISTED_IPS:
+        rules = add_rules(ip) # Blocks the ip from ufw
+        cached_rules[ip] = []
+
+    end = time.time()
+    print("Time elapsed:", end - start)
+    print("-----------------------------------------\n")
+
+
+def log_event(ip, event, rule, ip_details):
+    """
+        Logs an event. 
+    """
+    data = {
+        "timestamp": str(datetime.now()),
+        "src_ip": ip,
+        "event": event,
+        "rule": rule,
+        "ip_details": ip_details
+    }
+    with open('log/rttd_logs.txt', 'a') as f:
+        f.write(json.dumps(data) + "\n")
+    f.close()
+    print("Saved log\n")
+
+def load_last_event(path):
+    '''
+        Loads the IP from the last event. 
+    '''
+    data=[]
+    with open(path) as file:
+        for line in file: pass
+        if len(line) > 2:
+            # If the first and second to last is not char is not '{' and '}' respectively, the line is not fully written yet.
+            if line[0] == "{" and line[-2] == "}": 
+                data = json.loads(line)['src_ip']
+                return data
+    return None
+
+def add_rules(ip):
+    """
+    Adds firewall rules based on an input IP and logs this event.
+    Additionally, this function calls the IpAnalyzer in order to gather detailed information.
+    This is run in a new thread to not influence the performance of RTTD.
+    """
+    def task(ip):
+        rules = [f"deny from {ip} to any port 80", f"deny from {ip} to any port 443" ]
+        for rule in rules:
+            ufw.add(rule)
+            print("ADD", rule, "\n")
+        ip_details = IpAnalyzer().run(ip)
+        for rule in rules:
+            log_event(ip, "ADD", rule, ip_details)
+
+    thread = Thread(target=task, args=(ip,))
+    thread.start()
+
+
+def cleanup():
+    print("---------------- CLEANUP ----------------")
+    start = time.time()
+
+    ip_timestamps = load_honeypot_data('/home/cowrie/cowrie/var/log/cowrie/cowrie.json')
+    ips_24hours = get_ips_last_24hours(ip_timestamps)
+    ips_to_be_deleted = get_rules_tbd(ips_24hours)
+    delete_rules(ips_to_be_deleted)
+
+    end = time.time()
+    print(f"Deleted {len(ips_to_be_deleted)} rules")
+    print("Time elapsed:", end - start)
+    print("------------- CLEANED RULES -------------\n")
+
+def load_honeypot_data(path):
     '''
         Retrieves all IPs seen within the last two days. 
     '''
     data = []
 
     # Retrieves the honeypot data from today
-    for line in open('/home/cowrie/cowrie/var/log/cowrie/cowrie.json', 'r'):
+    for line in open(path, 'r'):
         data.append(json.loads(line))
 
     # Retrieves the honeypot data from yesterday
-    yesterday = get_yesterday()
-    yesterday_file_path = f'/home/cowrie/cowrie/var/log/cowrie/cowrie.json.{yesterday}'
+    yesterday = get_date_of_yesterday()
+    yesterday_file_path = f'{path}.{yesterday}'
     for line in open(yesterday_file_path, 'r'):
         data.append(json.loads(line))
 
@@ -29,7 +158,7 @@ def load_honeypot_data():
     return ip_timestamps
 
 
-def get_yesterday(frmt='%Y-%m-%d', string=True):
+def get_date_of_yesterday(frmt='%Y-%m-%d', string=True):
     '''
         Retrieves the date of yesterday. 
     '''
@@ -39,93 +168,84 @@ def get_yesterday(frmt='%Y-%m-%d', string=True):
     return yesterday
 
 
-def get_add_delete():
+def get_ips_last_24hours(ip_timestamps):
     '''
-        Retrieves two lists, one containing IPs to be added, one containing IPs to be deleted from the firewall. 
+        Retrieves a list containing IPs to be deleted from the firewall. 
     '''
-    ip_timestamps = load_honeypot_data()
-    to_be_added = set()
     to_be_deleted = set()
-
     now = datetime.now()
 
     for ip, timestamp in ip_timestamps.items(): 
         datetime_object = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
 
         # Checks if the IP has accessed the honeypot within the last 24 hours
-        if now-timedelta(hours=24) <= datetime_object <= now: 
-            #print(f"ADD RULE: {ip}")
-            to_be_added.add(ip)
+        if now-timedelta(minutes=1) <= datetime_object <= now: 
+            pass
         else: 
-            #print(f"DELETE RULE: {ip}")
             to_be_deleted.add(ip)
-    return to_be_added, to_be_deleted
-
-
-def get_firewall_rules():
-    '''
-        Retrieves all firewall rules currently active.
-    '''
-    return ufw.get_rules()
+    return to_be_deleted
 
 
 def get_rules_tbd(tbd): 
     '''
-        Retrieves the rules which should be deleted.
+        Retrieves the rules which should be deleted by cross-referencing the honeypot data with the active ufw rules.
     '''
-    active_rules = get_firewall_rules() 
+    active_rules = ufw.get_rules()
     to_be_deleted = set()
 
     # Find the rule numbers of the rules which needs to be deleted
-    # University IP is whitelisted 130.208.240.12
     for number, rule in active_rules.items(): 
-        if any(ip in rule for ip in tbd if not '130.208.240.12'):
+        if any((x:=ip) in rule for ip in tbd if ip not in WHITELISTED_IPS):
             # todo: check if the ip is the university ip 
-            to_be_deleted.add(number)
-
-    before_length = len(tbd)
-    after_length = len(to_be_deleted)
-    print(f"Out of {before_length} rules to delete, {after_length} were currently active")
-
+            to_be_deleted.add((x, number, rule))
+        
     return to_be_deleted
 
-
-def delete_rules(ips): 
+def delete_rules(rules): 
     '''
         Deletes firewall rules based on an input IP list.
     ''' 
-    rules = get_rules_tbd(ips)
-    for rule in rules: ufw.delete(rule)
+    for rule in rules: 
+        ufw.delete(rule[2])
+        print("DELETE:", rule[2])
+        log_event(rule[0], "DELETE", rule[2], {})
 
-def add_rules(ips):
-    '''
-        Adds firewall rules based on an input IP list. If they're already in the firewall, they won't be duplicated. 
-    '''
-    for ip in ips: 
-        ufw.add(f"deny from {ip} to any port 80")
-        ufw.add(f"deny from {ip} to any port 443")
+def reset_firewall():
+    print("------------ RESET FIREWALL -------------")
+    ufw.reset()
+    ufw.enable() 
+    ufw.default(incoming='allow', outgoing='allow', routed='reject')
+    ufw.set_logging('low')
+    print(ufw.status())    
+    print("-----------------------------------------\n")
 
-def main(): 
-    '''
-        Runs the script every 5 minutes.
-    '''
-    schedule.every(5).minutes.do(update_rules)
 
-    while True: 
-        schedule.run_pending()
-        time.sleep(1)
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    event_handler = MyEventHandler(path)
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    
+    schedule.every(30).minutes.do(update_cached_rules)
+    schedule.every(5).minutes.do(cleanup)
 
-def update_rules():
-    '''
-        Retrieves the IPs seen in the honeypot over the last 24 hours,
-        then compares them with the rules currently active on ufw to be able to add and delete them accordingly.   
-    '''
-    start = time.time()
-    add, delete = get_add_delete() 
-    delete_rules(delete) 
-    add_rules(add)
-    end = time.time()
-    print("Time:", end - start)
+    api_key = os.environ.get('IPHUB_KEY', None)
+    if not api_key:
+        raise Exception("Missing environment variable: IPHUB_KEY")
+    reset_firewall()
+    cleanup()
+    update_cached_rules()
 
-if __name__=="__main__":
-    main()
+    try:
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+            # check time treshold, if reached a certain time then update cache 
+    finally:
+        observer.stop()
+        observer.join()
