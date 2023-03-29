71bd60388cebb30f444e64bb985751febdc60fcb
Jonas Hagg <jonas.hagg@tum.de>
2023-03-29 12:54:40 +0000

    store generated graphs locally

--- analysis/charts/chart_generator.py
+++ analysis/charts/chart_generator.py
@@ -11,32 +11,56 @@ Edit:
 https://quickchart.io/chart-maker/edit/<id>
 """
 
+def save_image(image, filename):
+    """Store image on file system"""
+    with open(filename, "wb") as f:
+        f.write(image)
+
+def show_image(image):
+    """Show image using Pillow"""
+    img = Image.open(BytesIO(image))
+    img.show()
+
 class VerticalBarChart:
     @staticmethod
-    def render(label, data):
-        template = "https://quickchart.io/chart/render/zm-24e4e523-3e27-40a5-b559-e33f80461eaa"
+    def render(label, data, filename=None, show=None):
+        template = "https://quickchart.io/chart/render/zm-73e8fb5b-c54c-465d-9139-86b308c87122"
         params = {
             "data1": ",".join([str(val) for val in data]),
             "labels": ",".join(label)
         }
         response = requests.get(template, params)
-        img = Image.open(BytesIO(response.content))
-        img.show()
+        assert response.ok, "Error requesting quickchart API"
+
+        if show:
+            show_image(response.content)
+
+        if filename:
+            save_image(response.content, filename)
+
+        return response.content
 
 class PieChart:
+
     @staticmethod
-    def render(label, data):
-        BASE = "https://quickchart.io/chart/render/zm-b82a226a-3f63-4efb-b26a-c4007a69635a"
+    def render(label, data, filename=None, show=None):
+        BASE = "https://quickchart.io/chart/render/zm-f451639b-d854-4e3f-b3f6-f2dbdb7f0aa6"
         params = {
             "data1": ",".join([str(val) for val in data]),
             "labels": ",".join(label)
         }
-        print(params)
         response = requests.get(BASE, params)
-        img = Image.open(BytesIO(response.content))
-        img.show()
+        assert response.ok, "Error requesting quickchart API"
+
+        if show:
+            show_image(response.content)
+
+        if filename:
+            save_image(response.content, filename)
+
+        return response.content
 
 
 if __name__ == "__main__":
-    VerticalBarChart.render(["a", "b"], [10, 30])
-    PieChart.render(["a","b"], [20,80])
+    VerticalBarChart.render(["a", "b"], [10, 30], show=True)
+    PieChart.render(["a","b"], [20,80], show=True)
